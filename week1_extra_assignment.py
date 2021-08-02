536621409726fc8526dc3f5253489446ecdd4489
Michael McGarrygit config --global user.email mcgarry72@gmail.com <86483905+mcgarry72@users.noreply.github.com>
2021-08-02 13:47:08 -0400

    Program for extra assignment week 1, 450

---
+++ week1_extra_assignment.py
@@ -0,0 +1,11 @@
+# test problem
+
+verse = "If you can keep your head when all about you\n  Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n  But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n  Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n  And yet don’t look too good, nor talk too wise:"
+print(len(verse))
+print(verse.index("and"))
+
+print(verse[verse.index("and"):verse.rindex("and")])
+
+print(verse.rindex("and"))
+print(verse.count("you"))
+print(verse.count("mike"))
